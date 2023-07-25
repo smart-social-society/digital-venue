2f177b4f7718301c0298d4b2e6e0a2eb5b798f5e
Vihari Pattapu <viharipattapu@Viharis-MacBook-Pro.local>
2023-07-24 20:22:22 -0400

    Initial Component Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CareersComponent } from './pages/careers/careers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    ProductsComponent,
+    CareersComponent
   ],
   imports: [
     BrowserModule,
